75c6aac7989c7d0efec5ff489076bf0faf6415f2
YASHBHAKTA <19bmiit046@gmail.com>
2022-09-01 12:46:27 +0530

    FOOTER CHANGE

--- front-end/src/footer.js
+++ front-end/src/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 const footer =()=>{
     return(
         <div className="footer">
-            <h3>E-comm Dashboard YASH BHAKTA</h3>
+            <h3>E-comm Dashboard YASH BHAKTA & MANAS PATEL</h3>
         </div>
     )
 }
